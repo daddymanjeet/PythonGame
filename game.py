7ecb1d322fd54bcd95dfecafb11dc5055c756feb
Manjeet Dhaterwal <67642088+manjeetdhaterwal@users.noreply.github.com>
2021-01-28 10:58:50 +0530

    fixing bugs

--- game.py
+++ game.py
@@ -1,22 +1,22 @@
-def validate(hand)
-    if hand  0 or hand  2
+def validate(hand):
+    if hand < 0 or hand > 2:
         return False
     return True
 
-def print_hand(hand, name='Guest')
+def print_hand(hand, name='Guest'):
     hands = ['Rock', 'Paper', 'Scissors']
     print(name + ' picked ' + hands[hand])
 
-def judge(player, computer)
-    if player == computer
+def judge(player, computer):
+    if player == computer:
         return 'Draw'
-    elif player == 0 and computer == 1
+    elif player == 0 and computer == 1:
         return 'Lose'
-    elif player == 1 and computer == 2
+    elif player == 1 and computer == 2:
         return 'Lose'
-    elif player == 2 and computer == 0
+    elif player == 2 and computer == 0:
         return 'Lose'
-    else
+    else:
         return 'Win'
 # import utils
 # import the random module
@@ -28,7 +28,7 @@ player_name = input('Please enter your name ')
 print('Pick a hand (0 Rock, 1 Paper, 2 Scissors)')
 player_hand = int(input('Please enter a number (0-2) '))
 
-if validate(player_hand)
+if validate(player_hand):
     # Assign a random number between 0 and 2 to computer_hand using randint
     computer_hand = random.randint(0,2)
 
@@ -37,5 +37,5 @@ if validate(player_hand)
 
     result =judge(player_hand, computer_hand)
     print('Result ' + result)
-else
+else:
     print('Please enter a valid number')
